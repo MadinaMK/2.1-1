c803426bddc087bb5c5760ed1324901eab6bd474
Madina Karazhanova <karazhanovam@mail.ru>
2021-09-26 00:24:11 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 200;
+        int oneMile = 20;
+        int mileTotal = ticketPrice/20;
+        System.out.println(mileTotal);
+    }
+}
